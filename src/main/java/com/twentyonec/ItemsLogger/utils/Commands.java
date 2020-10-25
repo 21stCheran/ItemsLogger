67caab8277109cf6dd69c6fd0b71ffe8e790f9b7
21stCheran <43900990+21stCheran@users.noreply.github.com>
2020-10-26 01:02:29 +0530

    Command Utils

--- src/main/java/com/twentyonec/ItemsLogger/utils/Commands.java
+++ src/main/java/com/twentyonec/ItemsLogger/utils/Commands.java
@@ -1,22 +1,45 @@
 package com.twentyonec.ItemsLogger.utils;
 
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.twentyonec.ItemsLogger.ItemsLogger;
 
 public class Commands implements CommandExecutor {
-	
+
 	ItemsLogger plugin = ItemsLogger.getPlugin();
+	Storage storage = Storage.getStorage(plugin);
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-		plugin.debugMessage("command: " + command);
-		plugin.debugMessage("label: " + label);
-		plugin.debugMessage("args: " + args.toString());
+		if (!(sender instanceof Player))
+			return false;
+		if (args.length < 1)
+			return false;
+
+		Player target = (Bukkit.getServer().getPlayer(args[0]));
+		if (target == null) {
+			sender.sendMessage("§8[§cItemsLogger§8] §e" + args[0] + " is not online!");
+			return false;
+		}
 		
+		CommandsUtil commandsUtil;
+
+		if (args.length == 1) {
+			commandsUtil = new CommandsUtil(target.getUniqueId());
+			Arrays.asList(commandsUtil.retrieveList()).forEach(v -> {
+				sender.sendMessage( v.toString());
+			});;
+
+
+		}
+
 		return true;
 	}
 
