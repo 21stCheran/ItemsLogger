85dc7986cc56326d4365ac999bc14b482124e68a
21stCheran <43900990+21stCheran@users.noreply.github.com>
2020-10-26 14:28:45 +0530

    Chat Handler

--- src/main/java/com/twentyonec/ItemsLogger/commands/Commands.java
+++ src/main/java/com/twentyonec/ItemsLogger/commands/Commands.java
@@ -1,4 +1,4 @@
-package com.twentyonec.ItemsLogger.utils;
+package com.twentyonec.ItemsLogger.commands;
 
 import java.util.Arrays;
 
@@ -9,6 +9,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.twentyonec.ItemsLogger.ItemsLogger;
+import com.twentyonec.ItemsLogger.utils.Storage;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 
 public class Commands implements CommandExecutor {
 
@@ -28,13 +34,21 @@ public class Commands implements CommandExecutor {
 			sender.sendMessage("§8[§cItemsLogger§8] §e" + args[0] + " is not online!");
 			return false;
 		}
-		
+
 		CommandsUtil commandsUtil;
 
 		if (args.length == 1) {
 			commandsUtil = new CommandsUtil(target.getUniqueId());
 			Arrays.asList(commandsUtil.retrieveList()).forEach(v -> {
-				sender.sendMessage( v.toString());
+
+				TextComponent message = new TextComponent(v.toString());
+				message.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, 
+						new Text("21C is cool")));
+				message.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, 
+						"/plugins" ) );
+
+				
+				sender.spigot().sendMessage(message);
 			});;
 
 
