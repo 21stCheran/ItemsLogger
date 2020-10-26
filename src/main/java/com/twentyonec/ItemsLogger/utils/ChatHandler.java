3aa84b24844f08147133f5ff227f35f2ed943463
21stCheran <43900990+21stCheran@users.noreply.github.com>
2020-10-26 16:53:47 +0530

    SQL cleanup

--- src/main/java/com/twentyonec/ItemsLogger/utils/ChatHandler.java
+++ src/main/java/com/twentyonec/ItemsLogger/utils/ChatHandler.java
@@ -1,5 +1,38 @@
 package com.twentyonec.ItemsLogger.utils;
 
+import com.twentyonec.ItemsLogger.ItemPlayer;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+
 public class ChatHandler {
+
+	public static TextComponent[] sendData(ItemPlayer[] playerlist, String date, String cause) {
+
+		if (date == null)
+			date = "Most Recent";
+		if (cause == null)
+			cause = "All";
+
+		TextComponent[] components = new TextComponent[11];
+		components[0] = new TextComponent("§8[§cItemsLogger§8] \n§6Date §7" + date + "\n§6Causes: §7" + cause);
+
+		for (int i = 0; i < playerlist.length; i++) {
+
+			TextComponent message = new TextComponent("\n§9Date: §7 + " 
+					+ playerlist[i].date.toString()
+					+ " "
+					+ playerlist[i].time.toString());
+			message.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, 
+					"/plugins" ) );
+			message.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, 
+					new Text( "§cView Info" ) ) );
+			components[i + 1] = message;
+		}
+
+		return components;
+	}
 
 }
